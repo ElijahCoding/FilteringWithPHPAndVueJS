43136f0e84ea432ce34834512959bcdd43d412fc
Elijah <elijahsharper1@gmail.com>
2018-01-24 12:49:20 +0300

    ResourceCollection and Endpoint set-up

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Course;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CourseResource;
 
 class CourseController extends Controller
 {
   public function index(Request $request)
   {
-    $courses =  Course::with(['subjects'])->filter($request, $this->getFilters())->get();
-
-    return response()->json($courses, 200);
+    return new CourseResource(
+      Course::with(['subjects'])->filter($request, $this->getFilters())->paginate(5)
+    );
   }
 
   protected function getFilters()
