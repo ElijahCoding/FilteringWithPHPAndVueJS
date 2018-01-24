887feb415c7bf64cb6afdf90bfe269ca127114f4
Elijah <elijahsharper1@gmail.com>
2018-01-24 23:02:03 +0300

    Removing Filters and Refactorying

--- public/js/app.js
+++ public/js/app.js
@@ -49663,12 +49663,16 @@ module.exports = Component.exports
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__partials_Course_vue__ = __webpack_require__(43);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__partials_Course_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__partials_Course_vue__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__pagination_Pagination_vue__ = __webpack_require__(52);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__pagination_Pagination_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__pagination_Pagination_vue__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__partials_Filters_vue__ = __webpack_require__(55);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__partials_Filters_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__partials_Filters_vue__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_query_string__ = __webpack_require__(58);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_query_string___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_query_string__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__partials_Course__ = __webpack_require__(43);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__partials_Course___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__partials_Course__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__pagination_Pagination__ = __webpack_require__(52);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__pagination_Pagination___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2__pagination_Pagination__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__partials_Filters__ = __webpack_require__(55);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__partials_Filters___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_3__partials_Filters__);
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 //
 //
 //
@@ -49686,45 +49690,60 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+
 
 
 
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-  components: {
-    Course: __WEBPACK_IMPORTED_MODULE_0__partials_Course_vue___default.a,
-    Pagination: __WEBPACK_IMPORTED_MODULE_1__pagination_Pagination_vue___default.a,
-    Filters: __WEBPACK_IMPORTED_MODULE_2__partials_Filters_vue___default.a
-  },
+    components: {
+        Course: __WEBPACK_IMPORTED_MODULE_1__partials_Course___default.a,
+        Pagination: __WEBPACK_IMPORTED_MODULE_2__pagination_Pagination___default.a,
+        Filters: __WEBPACK_IMPORTED_MODULE_3__partials_Filters___default.a
+    },
+    data: function data() {
+        return {
+            courses: [],
+            meta: null
+        };
+    },
+    mounted: function mounted() {
+        this.getCourses();
+    },
 
-  data: function data() {
-    return {
-      courses: [],
-      meta: {}
-    };
-  },
-  mounted: function mounted() {
-    this.getCourses();
-  },
+    watch: {
+        '$route.query': {
+            handler: function handler(query) {
+                this.getCourses(1, query);
+            },
 
-
-  methods: {
-    getCourses: function getCourses() {
-      var _this = this;
-
-      var page = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.$route.query.page;
-
-      axios.get('/api/courses', {
-        params: {
-          page: page
+            deep: true
         }
-      }).then(function (response) {
-        _this.courses = response.data.data;
-        _this.meta = response.data.meta;
-      });
+    },
+    methods: {
+        getCourses: function getCourses() {
+            var _this = this;
+
+            var page = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.$route.query.page;
+            var filters = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : this.$route.query;
+
+            axios.get('/api/courses', {
+                params: _extends({
+                    page: page
+                }, filters)
+            }).then(function (response) {
+                _this.courses = response.data.data;
+                _this.meta = response.data.meta;
+            });
+        }
     }
-  }
 });
 
 /***/ }),
@@ -49889,14 +49908,21 @@ var render = function() {
           "div",
           { staticClass: "card-body" },
           [
-            _vm._l(_vm.courses, function(course) {
-              return _c("course", { key: course.id, attrs: { course: course } })
-            }),
-            _vm._v(" "),
-            _c("pagination", {
-              attrs: { meta: _vm.meta },
-              on: { "pagination:switched": _vm.getCourses }
-            })
+            _vm.courses.length
+              ? [
+                  _vm._l(_vm.courses, function(course) {
+                    return _c("course", {
+                      key: course.id,
+                      attrs: { course: course }
+                    })
+                  }),
+                  _vm._v(" "),
+                  _c("pagination", {
+                    attrs: { meta: _vm.meta },
+                    on: { "pagination:switched": _vm.getCourses }
+                  })
+                ]
+              : [_vm._v("\n          No results found\n        ")]
           ],
           2
         )
@@ -50343,6 +50369,328 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
         }
     }
 });
+
+/***/ }),
+/* 58 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var strictUriEncode = __webpack_require__(59);
+var objectAssign = __webpack_require__(60);
+
+function encoderForArrayFormat(opts) {
+	switch (opts.arrayFormat) {
+		case 'index':
+			return function (key, value, index) {
+				return value === null ? [
+					encode(key, opts),
+					'[',
+					index,
+					']'
+				].join('') : [
+					encode(key, opts),
+					'[',
+					encode(index, opts),
+					']=',
+					encode(value, opts)
+				].join('');
+			};
+
+		case 'bracket':
+			return function (key, value) {
+				return value === null ? encode(key, opts) : [
+					encode(key, opts),
+					'[]=',
+					encode(value, opts)
+				].join('');
+			};
+
+		default:
+			return function (key, value) {
+				return value === null ? encode(key, opts) : [
+					encode(key, opts),
+					'=',
+					encode(value, opts)
+				].join('');
+			};
+	}
+}
+
+function parserForArrayFormat(opts) {
+	var result;
+
+	switch (opts.arrayFormat) {
+		case 'index':
+			return function (key, value, accumulator) {
+				result = /\[(\d*)\]$/.exec(key);
+
+				key = key.replace(/\[\d*\]$/, '');
+
+				if (!result) {
+					accumulator[key] = value;
+					return;
+				}
+
+				if (accumulator[key] === undefined) {
+					accumulator[key] = {};
+				}
+
+				accumulator[key][result[1]] = value;
+			};
+
+		case 'bracket':
+			return function (key, value, accumulator) {
+				result = /(\[\])$/.exec(key);
+				key = key.replace(/\[\]$/, '');
+
+				if (!result) {
+					accumulator[key] = value;
+					return;
+				} else if (accumulator[key] === undefined) {
+					accumulator[key] = [value];
+					return;
+				}
+
+				accumulator[key] = [].concat(accumulator[key], value);
+			};
+
+		default:
+			return function (key, value, accumulator) {
+				if (accumulator[key] === undefined) {
+					accumulator[key] = value;
+					return;
+				}
+
+				accumulator[key] = [].concat(accumulator[key], value);
+			};
+	}
+}
+
+function encode(value, opts) {
+	if (opts.encode) {
+		return opts.strict ? strictUriEncode(value) : encodeURIComponent(value);
+	}
+
+	return value;
+}
+
+function keysSorter(input) {
+	if (Array.isArray(input)) {
+		return input.sort();
+	} else if (typeof input === 'object') {
+		return keysSorter(Object.keys(input)).sort(function (a, b) {
+			return Number(a) - Number(b);
+		}).map(function (key) {
+			return input[key];
+		});
+	}
+
+	return input;
+}
+
+exports.extract = function (str) {
+	return str.split('?')[1] || '';
+};
+
+exports.parse = function (str, opts) {
+	opts = objectAssign({arrayFormat: 'none'}, opts);
+
+	var formatter = parserForArrayFormat(opts);
+
+	// Create an object with no prototype
+	// https://github.com/sindresorhus/query-string/issues/47
+	var ret = Object.create(null);
+
+	if (typeof str !== 'string') {
+		return ret;
+	}
+
+	str = str.trim().replace(/^(\?|#|&)/, '');
+
+	if (!str) {
+		return ret;
+	}
+
+	str.split('&').forEach(function (param) {
+		var parts = param.replace(/\+/g, ' ').split('=');
+		// Firefox (pre 40) decodes `%3D` to `=`
+		// https://github.com/sindresorhus/query-string/pull/37
+		var key = parts.shift();
+		var val = parts.length > 0 ? parts.join('=') : undefined;
+
+		// missing `=` should be `null`:
+		// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+		val = val === undefined ? null : decodeURIComponent(val);
+
+		formatter(decodeURIComponent(key), val, ret);
+	});
+
+	return Object.keys(ret).sort().reduce(function (result, key) {
+		var val = ret[key];
+		if (Boolean(val) && typeof val === 'object' && !Array.isArray(val)) {
+			// Sort object keys, not values
+			result[key] = keysSorter(val);
+		} else {
+			result[key] = val;
+		}
+
+		return result;
+	}, Object.create(null));
+};
+
+exports.stringify = function (obj, opts) {
+	var defaults = {
+		encode: true,
+		strict: true,
+		arrayFormat: 'none'
+	};
+
+	opts = objectAssign(defaults, opts);
+
+	var formatter = encoderForArrayFormat(opts);
+
+	return obj ? Object.keys(obj).sort().map(function (key) {
+		var val = obj[key];
+
+		if (val === undefined) {
+			return '';
+		}
+
+		if (val === null) {
+			return encode(key, opts);
+		}
+
+		if (Array.isArray(val)) {
+			var result = [];
+
+			val.slice().forEach(function (val2) {
+				if (val2 === undefined) {
+					return;
+				}
+
+				result.push(formatter(key, val2, result.length));
+			});
+
+			return result.join('&');
+		}
+
+		return encode(key, opts) + '=' + encode(val, opts);
+	}).filter(function (x) {
+		return x.length > 0;
+	}).join('&') : '';
+};
+
+
+/***/ }),
+/* 59 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+module.exports = function (str) {
+	return encodeURIComponent(str).replace(/[!'()*]/g, function (c) {
+		return '%' + c.charCodeAt(0).toString(16).toUpperCase();
+	});
+};
+
+
+/***/ }),
+/* 60 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/*
+object-assign
+(c) Sindre Sorhus
+@license MIT
+*/
+
+
+/* eslint-disable no-unused-vars */
+var getOwnPropertySymbols = Object.getOwnPropertySymbols;
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+var propIsEnumerable = Object.prototype.propertyIsEnumerable;
+
+function toObject(val) {
+	if (val === null || val === undefined) {
+		throw new TypeError('Object.assign cannot be called with null or undefined');
+	}
+
+	return Object(val);
+}
+
+function shouldUseNative() {
+	try {
+		if (!Object.assign) {
+			return false;
+		}
+
+		// Detect buggy property enumeration order in older V8 versions.
+
+		// https://bugs.chromium.org/p/v8/issues/detail?id=4118
+		var test1 = new String('abc');  // eslint-disable-line no-new-wrappers
+		test1[5] = 'de';
+		if (Object.getOwnPropertyNames(test1)[0] === '5') {
+			return false;
+		}
+
+		// https://bugs.chromium.org/p/v8/issues/detail?id=3056
+		var test2 = {};
+		for (var i = 0; i < 10; i++) {
+			test2['_' + String.fromCharCode(i)] = i;
+		}
+		var order2 = Object.getOwnPropertyNames(test2).map(function (n) {
+			return test2[n];
+		});
+		if (order2.join('') !== '0123456789') {
+			return false;
+		}
+
+		// https://bugs.chromium.org/p/v8/issues/detail?id=3056
+		var test3 = {};
+		'abcdefghijklmnopqrst'.split('').forEach(function (letter) {
+			test3[letter] = letter;
+		});
+		if (Object.keys(Object.assign({}, test3)).join('') !==
+				'abcdefghijklmnopqrst') {
+			return false;
+		}
+
+		return true;
+	} catch (err) {
+		// We don't expect any of the above to throw, but better to be safe.
+		return false;
+	}
+}
+
+module.exports = shouldUseNative() ? Object.assign : function (target, source) {
+	var from;
+	var to = toObject(target);
+	var symbols;
+
+	for (var s = 1; s < arguments.length; s++) {
+		from = Object(arguments[s]);
+
+		for (var key in from) {
+			if (hasOwnProperty.call(from, key)) {
+				to[key] = from[key];
+			}
+		}
+
+		if (getOwnPropertySymbols) {
+			symbols = getOwnPropertySymbols(from);
+			for (var i = 0; i < symbols.length; i++) {
+				if (propIsEnumerable.call(from, symbols[i])) {
+					to[symbols[i]] = from[symbols[i]];
+				}
+			}
+		}
+	}
+
+	return to;
+};
+
 
 /***/ })
 /******/ ]);
--- resources/assets/js/components/courses/Index.vue
+++ resources/assets/js/components/courses/Index.vue
@@ -7,8 +7,14 @@
     <div class="col-sm-9">
       <div class="card card-default">
         <div class="card-body">
-          <course v-for="course in courses" :course="course" :key="course.id"></course>
-          <pagination :meta="meta" v-on:pagination:switched="getCourses"></pagination>
+          <template v-if="courses.length">
+            <course v-for="course in courses" :course="course" :key="course.id"></course>
+            <pagination :meta="meta" v-on:pagination:switched="getCourses"></pagination>
+          </template>
+
+          <template v-else>
+            No results found
+          </template>
         </div>
       </div>
     </div>
@@ -16,39 +22,46 @@
 </template>
 
 <script>
-  import Course from './partials/Course.vue';
-  import Pagination from '../pagination/Pagination.vue';
-  import Filters from './partials/Filters.vue';
+    import queryString from 'query-string'
+    import Course from './partials/Course'
+    import Pagination from '../pagination/Pagination'
+    import Filters from './partials/Filters'
 
-  export default {
-    components: {
-      Course,
-      Pagination,
-      Filters
-    },
-
-    data() {
-      return {
-        courses: [],
-        meta: {}
-      }
-    },
-
-    mounted () {
-      this.getCourses()
-    },
-
-    methods: {
-      getCourses (page = this.$route.query.page) {
-        axios.get('/api/courses', {
-          params: {
-            page: page
-          }
-        }).then((response) => {
-          this.courses = response.data.data
-          this.meta = response.data.meta
-        })
-      }
+    export default {
+        components: {
+            Course,
+            Pagination,
+            Filters
+        },
+        data () {
+            return {
+                courses: [],
+                meta: null
+            }
+        },
+        mounted () {
+            this.getCourses()
+        },
+        watch: {
+            '$route.query': {
+                handler (query) {
+                    this.getCourses(1, query)
+                },
+                deep: true
+            }
+        },
+        methods: {
+            getCourses(page = this.$route.query.page, filters = this.$route.query) {
+                axios.get('/api/courses', {
+                    params: {
+                        page,
+                        ...filters
+                    }
+                }).then((response) => {
+                    this.courses = response.data.data
+                    this.meta = response.data.meta
+                })
+            }
+        }
     }
-  }
 </script>
